b0f5b16987013c7f5dae863361818296923931ce
Olivier De Jonckere <olivierdejonckere@gmail.com>
2025-08-06 12:06:38 +0200

    isolate yaml case

---
+++ src/config/mod.rs
@@ -0,0 +1,71 @@
+use crate::{
+    config::yaml_vec::YamlVec, db::ChatDataBase, dtchat::ASabrInitState,
+    prediction::PredictionConfig,
+};
+use serde::Deserialize;
+use std::fs;
+
+mod yaml_vec;
+
+#[derive(Debug, Clone, Deserialize)]
+pub enum DbType {
+    YamlVec,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub db_type: DbType,
+    pub cp_path: Option<String>,
+}
+
+pub struct AppConfig {}
+
+impl AppConfig {
+    const DEFAULT_CONFIG_PATH_VALUE: &str = "default.yaml";
+    const DEFAULT_CONFIG_PATH_ENV_VAR: &str = "CONFIG_PATH";
+
+    pub fn new() -> (Box<dyn ChatDataBase>, ASabrInitState) {
+        let config_file = match std::env::var(Self::DEFAULT_CONFIG_PATH_ENV_VAR) {
+            Ok(path) => path,
+            Err(_) => {
+                println!(
+                    "{} is not set, trying with {}",
+                    Self::DEFAULT_CONFIG_PATH_ENV_VAR,
+                    Self::DEFAULT_CONFIG_PATH_VALUE
+                );
+                Self::DEFAULT_CONFIG_PATH_VALUE.to_string()
+            }
+        };
+
+        let conf: Config = Self::from_file(&config_file).unwrap_or_else(|e| {
+            panic!("Failed to load configuration from '{config_file}': {e}");
+        });
+
+        let db = match conf.db_type {
+            DbType::YamlVec => YamlVec::new(&config_file),
+        };
+        let cp_path_unwrapped = match conf.cp_path {
+            Some(cp) => cp,
+            None => {
+                return (db, ASabrInitState::Disabled);
+            }
+        };
+
+        let pred_res = PredictionConfig::try_init(cp_path_unwrapped);
+        let pred_opt = match pred_res {
+            Ok(pred_conf) => ASabrInitState::Enabled(pred_conf),
+            Err(err) => ASabrInitState::Error(err.to_string()),
+        };
+        (db, pred_opt)
+    }
+
+    pub fn from_file<T, P>(path: P) -> Result<T, Box<dyn std::error::Error>>
+    where
+        T: for<'de> Deserialize<'de>,
+        P: AsRef<std::path::Path>,
+    {
+        let content = fs::read_to_string(path)?;
+        let config: T = serde_yaml::from_str(&content)?;
+        Ok(config)
+    }
+}
--- src/config/yaml_vec.rs
+++ src/config/yaml_vec.rs
@@ -1,7 +1,7 @@
 use crate::{
+    config::AppConfig,
     db::{simple_vec::SimpleVecDB, ChatDataBase},
-    dtchat::{ASabrInitState, Peer},
-    prediction::PredictionConfig,
+    dtchat::Peer,
 };
 use serde::{
     de::{self, Visitor},
@@ -9,18 +9,7 @@ use serde::{
 };
 use socket_engine::endpoint::Endpoint;
 use std::fmt;
-use std::fs;
 
-#[derive(Debug, Clone, Deserialize)]
-pub enum DbType {
-    YamlVec,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Config {
-    pub db_type: DbType,
-    pub cp_path: Option<String>,
-}
 #[derive(Clone, Debug)]
 pub struct EndpointWrapper(pub Endpoint);
 
@@ -52,7 +41,6 @@ impl<'de> Deserialize<'de> for EndpointWrapper {
     }
 }
 
-// Optional: Convert from wrapper to inner type
 impl From<EndpointWrapper> for Endpoint {
     fn from(wrapper: EndpointWrapper) -> Self {
         wrapper.0
@@ -67,11 +55,6 @@ pub struct RawPeer {
     pub color: String,
 }
 
-#[derive(Debug, Deserialize)]
-pub struct YamlVec {
-    pub peer_list: Vec<RawPeer>,
-}
-
 impl From<RawPeer> for Peer {
     fn from(raw: RawPeer) -> Self {
         Peer {
@@ -83,48 +66,13 @@ impl From<RawPeer> for Peer {
     }
 }
 
-pub struct AppConfig {}
+#[derive(Debug, Deserialize)]
+pub struct YamlVec {
+    pub peer_list: Vec<RawPeer>,
+}
 
-impl AppConfig {
-    const DEFAULT_CONFIG_PATH_VALUE: &str = "default.yaml";
-    const DEFAULT_CONFIG_PATH_ENV_VAR: &str = "CONFIG_PATH";
-
-    pub fn new() -> (Box<dyn ChatDataBase>, ASabrInitState) {
-        let config_file = match std::env::var(Self::DEFAULT_CONFIG_PATH_ENV_VAR) {
-            Ok(path) => path,
-            Err(_) => {
-                println!(
-                    "{} is not set, trying with {}",
-                    Self::DEFAULT_CONFIG_PATH_ENV_VAR,
-                    Self::DEFAULT_CONFIG_PATH_VALUE
-                );
-                Self::DEFAULT_CONFIG_PATH_VALUE.to_string()
-            }
-        };
-
-        let conf: Config = Self::from_file(&config_file).unwrap_or_else(|e| {
-            panic!("Failed to load configuration from '{config_file}': {e}");
-        });
-
-        let db = match conf.db_type {
-            DbType::YamlVec => Self::yaml_vec(&config_file),
-        };
-        let cp_path_unwrapped = match conf.cp_path {
-            Some(cp) => cp,
-            None => {
-                return (db, ASabrInitState::Disabled);
-            }
-        };
-
-        let pred_res = PredictionConfig::try_init(cp_path_unwrapped);
-        let pred_opt = match pred_res {
-            Ok(pred_conf) => ASabrInitState::Enabled(pred_conf),
-            Err(err) => ASabrInitState::Error(err.to_string()),
-        };
-        (db, pred_opt)
-    }
-
-    fn yaml_vec(config_file: &str) -> Box<dyn ChatDataBase> {
+impl YamlVec {
+    pub fn new(config_file: &str) -> Box<dyn ChatDataBase> {
         const PEER_ENV_VAR: &str = "PEER_UUID";
 
         let local_peer_uuid = match std::env::var(PEER_ENV_VAR) {
@@ -134,7 +82,7 @@ impl AppConfig {
             }
         };
 
-        let conf: YamlVec = Self::from_file(&config_file).unwrap_or_else(|e| {
+        let conf: YamlVec = AppConfig::from_file(&config_file).unwrap_or_else(|e| {
             panic!("Failed to load configuration from '{config_file}': {e}");
         });
 
@@ -152,15 +100,5 @@ impl AppConfig {
         };
 
         Box::new(SimpleVecDB::new(Vec::new(), Peer::from(local_peer), peers))
-    }
-
-    pub fn from_file<T, P>(path: P) -> Result<T, Box<dyn std::error::Error>>
-    where
-        T: for<'de> Deserialize<'de>,
-        P: AsRef<std::path::Path>,
-    {
-        let content = fs::read_to_string(path)?;
-        let config: T = serde_yaml::from_str(&content)?;
-        Ok(config)
     }
 }
